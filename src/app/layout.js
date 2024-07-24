7a5f3f2764c6d8076a820744770686080a618aea
TusharDC <tushar@digitalcappuccino.com>
2024-07-24 15:42:16 +0530

    noscript added

--- src/app/layout.js
+++ src/app/layout.js
@@ -54,7 +54,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-T5V6TDPX')`}
                 </Script> */}
-                <GoogleTagManager gtmId="T5V6TDPX" />
+                <GoogleTagManager gtmId="GTM-T5V6TDPX" />
                 {/* <Script
                     strategy="beforeInteractive"
                     id="analytics-conversion-four"
---
+++ src/pages/_document.js
@@ -0,0 +1,21 @@
+import Document, { NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <body>
+                <NextScript />
+                <noscript
+                    dangerouslySetInnerHTML={{
+                        __html: `<iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-T5V6TDPX"
+                        height="0"
+                        width="0"
+                        style="display:none;visibility:hidden"
+                    ></iframe>`,
+                    }}
+                />
+            </body>
+        );
+    }
+}
