2ef86a35b7e97727ffff41103222837939e3b514
fastestsunil <sunil141292@gmail.com>
2024-09-12 15:09:32 +0530

    form resposnvie in mobile

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -14,26 +14,18 @@ const Banner = () => {
         }}
       >
         {' '}
-        <div className='font-bold text-4xl text-center text-white pt-20'>
+        <div className="font-bold text-4xl text-center text-white pt-20">
           Instant Loans, from $100 to $5000
         </div>
-        <div className='text-2xl text-center font-semibold text-white pt-5' >
+        <div className="text-2xl text-center font-semibold text-white pt-5">
           All Credit Types Welcome
         </div>
         <div className="iframe-container">
-          <iframe
-            title="Frame Content"
-            src={frameSrc}
-            className=''
-          >
-
-          </iframe>
+          <iframe title="Frame Content" src={frameSrc} className=""></iframe>
           {/* <div className='text-2xl text-center font-semibold text-white pt-5'>
             Representative Example: $1,000 loan over a 12-month term would have a total cost, including interest, a total payback amount of $1,134.72. APR 29.82%. Rates between 4.95% APR and 34.95% APR. Loan term lengths from 61 days to 72 months for qualified consumers.
           </div> */}
         </div>
-
-
         {/* <div className="flex flex-col items-center h-full backdrop-blur-sm bg-black/40">
           <h1 className="font-bold text-5xl text-white mt-24 w-[60%] text-center leading-[55px]">
             {' '}
