38b5052c992446b6aba365988fd9f3a101812489
TusharDC <tushar@digitalcappuccino.com>
2024-09-24 16:04:18 +0530

    tags added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,50 @@ import Document, { NextScript } from "next/document";
 export default class MyDocument extends Document {
     render() {
         return (
+            <>
+             <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>form</title>
+        <style>
+            #omForm-root {
+                margin: 0px !important;
+            }
+
+            #omForm-root > * {
+                min-height: 0px !important;
+            }
+        </style>
+        <!-- Google tag (gtag.js) -->
+        <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-E4G675K7K8"
+        ></script>
+        <!-- Google tag (gtag.js) -->
+        <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=AW-16631389480"
+        ></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag("js", new Date());
+
+            gtag("config", "AW-16631389480");
+        </script>
+
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag("js", new Date());
+
+            gtag("config", "G-E4G675K7K8");
+        </script>
+    </head>
             <body>
                 <NextScript />
                 <noscript
@@ -16,6 +60,6 @@ export default class MyDocument extends Document {
                     }}
                 />
             </body>
-        );
+</>        );
     }
 }
