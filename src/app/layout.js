5f99332984b1820b3912d5b36b3e7cb0a5e0a9b5
TusharDC <tushar@digitalcappuccino.com>
2024-09-24 15:26:41 +0530

    chnages

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="enn">
+        <html lang="en">
             <body className={inter.className}>
                 {/* <Test /> */}
                 <Navbar />
