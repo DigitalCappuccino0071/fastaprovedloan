fed165210e9ac54d4da901fe8286c14d9dc8e51c
fastestsunil <sunil141292@gmail.com>
2025-01-20 12:06:43 +0530

    Update @next/third-parties to version 14.2.23 and refactor Google Tag Manager integration
    - Updated the version of @next/third-parties in package.json and package-lock.json.
    - Refactored layout.js to include Google Analytics and a single instance of Google Tag Manager.
    - Updated _document.js to reflect the new Google Tag Manager ID and cleaned up the noscript tag.
    
    This commit enhances tracking capabilities and ensures dependencies are up to date.

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,25 +3,22 @@ import MaterialDisclaimer from '@/components/common/MaterialDisclaimer';
 import Navbar from '@/components/common/Navbar';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { GoogleTagManager } from '@next/third-parties/google';
+
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-5VQDJQ8F" />
       <body className={inter.className}>
         {/* <Test /> */}
         <Navbar />
         {children}
         <MaterialDisclaimer />
         <Footer />
-
-        {/* <GoogleTagManager gtmId="GTM-T5V6TDPX" /> */}
-        {/* <GoogleTagManager gtmId="GTM-WNGZK579" /> */}
-        <GoogleTagManager gtmId="GTM-WNGZK579" />
-        <GoogleTagManager gtmId="AW-16631389480" />
-        <GoogleTagManager gtmId="G-E4G675K7K8" />
+        <GoogleAnalytics gaId="AW-11559522543" />
       </body>
     </html>
   );
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,10 +5,20 @@ export default class MyDocument extends Document {
     return (
       <body>
         <NextScript />
-        <noscript
+        {/* <noscript
           dangerouslySetInnerHTML={{
             __html: `<iframe
               src="https://www.googletagmanager.com/ns.html?id=GTM-WNGZK579"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>`,
+          }}
+        /> */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5VQDJQ8F"
               height="0"
               width="0"
               style="display:none;visibility:hidden"
